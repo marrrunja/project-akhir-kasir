34597e3ef7cb0792f396e1b1f7c15da7efd2484e
marrrunja <muammarirfan21@gmail.com>
2025-06-09 14:50:25 +0700

    nothing, form

---
+++ project-akhir-kasir/Config/Database.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace project_akhir_kasir.Config
+{
+    public static class Database
+    {
+        private static String dbServer = "localhost";
+        private static String dbUser = "root";
+        private static String dbPassword = "";
+        private static String dbName = "project_pemvis";
+
+        public static String ConnStr => $"server={dbServer};user={dbUser}; database={dbName}; password={dbPassword}";
+    }
+}
--- project-akhir-kasir/Form1.Designer.cs
+++ project-akhir-kasir/Form1.Designer.cs
@@ -28,12 +28,81 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            button1 = new Button();
+            textboxPassword = new RichTextBox();
+            textboxUsername = new RichTextBox();
+            labelUsername = new Label();
+            labelPassword = new Label();
+            SuspendLayout();
+            // 
+            // button1
+            // 
+            button1.Location = new Point(294, 370);
+            button1.Name = "button1";
+            button1.Size = new Size(419, 65);
+            button1.TabIndex = 2;
+            button1.Text = "button1";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // textboxPassword
+            // 
+            textboxPassword.Location = new Point(294, 280);
+            textboxPassword.Name = "textboxPassword";
+            textboxPassword.Size = new Size(426, 67);
+            textboxPassword.TabIndex = 3;
+            textboxPassword.Text = "";
+            // 
+            // textboxUsername
+            // 
+            textboxUsername.Location = new Point(294, 145);
+            textboxUsername.Name = "textboxUsername";
+            textboxUsername.Size = new Size(426, 67);
+            textboxUsername.TabIndex = 4;
+            textboxUsername.Text = "";
+            // 
+            // labelUsername
+            // 
+            labelUsername.AutoSize = true;
+            labelUsername.Font = new Font("Segoe UI Semibold", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            labelUsername.Location = new Point(294, 99);
+            labelUsername.Name = "labelUsername";
+            labelUsername.Size = new Size(124, 32);
+            labelUsername.TabIndex = 5;
+            labelUsername.Text = "Username";
+            // 
+            // labelPassword
+            // 
+            labelPassword.AutoSize = true;
+            labelPassword.Font = new Font("Segoe UI Semibold", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            labelPassword.Location = new Point(294, 230);
+            labelPassword.Name = "labelPassword";
+            labelPassword.Size = new Size(124, 32);
+            labelPassword.TabIndex = 6;
+            labelPassword.Text = "Username";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.White;
+            ClientSize = new Size(943, 523);
+            Controls.Add(labelPassword);
+            Controls.Add(labelUsername);
+            Controls.Add(textboxUsername);
+            Controls.Add(textboxPassword);
+            Controls.Add(button1);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Button button1;
+        private RichTextBox textboxPassword;
+        private RichTextBox textboxUsername;
+        private Label labelUsername;
+        private Label labelPassword;
     }
 }
